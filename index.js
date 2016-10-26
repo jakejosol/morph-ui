b09962ad45a9034f2694c99d96147e7196e6f551
Jake Josol <jmjosol@hotmail.com>
2016-10-26 20:37:13 +0800

    Fixed path

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 module.exports = {
 
      Templates: {
-        Page: require('./charts/templates/page')
+        Page: require('./components/charts/templates/page')
     },
 
      Charts: {
-        BarStack: require('./charts/bar-stack'),
-        LineGraph: require('./charts/line-graph'),
-        PieChart: require('./charts/pie-chart')
+        BarStack: require('./components/charts/bar-stack'),
+        LineGraph: require('./components/charts/line-graph'),
+        PieChart: require('./components/charts/pie-chart')
     },
 
      Form: {
-        Checkbox: require('./form/checkbox'),
-        CheckboxGroup: require('./form/checkbox-group'),
-        InputDate: require('./form/input-date'),
-        InputFile: require('./form/input-file'),
-        InputGroup: require('./form/input-group'),
-        InputMoney: require('./form/input-money'),
-        InputNumber: require('./form/input-number'),
-        Radio: require('./form/radio'),
-        RadioGroup: require('./form/radio-group'),
-        Textarea: require('./form/textarea')
+        Checkbox: require('./components/form/checkbox'),
+        CheckboxGroup: require('./components/form/checkbox-group'),
+        InputDate: require('./components/form/input-date'),
+        InputFile: require('./components/form/input-file'),
+        InputGroup: require('./components/form/input-group'),
+        InputMoney: require('./components/form/input-money'),
+        InputNumber: require('./components/form/input-number'),
+        Radio: require('./components/form/radio'),
+        RadioGroup: require('./components/form/radio-group'),
+        Textarea: require('./components/form/textarea')
     },
 
      Layout: {
-        AppBar: require('./layout/app-bar'),
-        Card: require('./form/card'),
-        CardLoader: require('./form/card-loader'),
-        Column: require('./form/column'),
-        Container: require('./form/container'),
-        Content: require('./form/content'),
-        InfoCard: require('./form/info-card'),
-        NavBar: require('./form/nav-bar'),
-        Side: require('./form/side'),
-        SideMenu: require('./form/side-menu'),
-        Table: require('./form/table')
+        AppBar: require('./components/layout/app-bar'),
+        Card: require('./components/form/card'),
+        CardLoader: require('./components/form/card-loader'),
+        Column: require('./components/form/column'),
+        Container: require('./components/form/container'),
+        Content: require('./components/form/content'),
+        InfoCard: require('./components/form/info-card'),
+        NavBar: require('./components/form/nav-bar'),
+        Side: require('./components/form/side'),
+        SideMenu: require('./components/form/side-menu'),
+        Table: require('./components/form/table')
     },
 
      Widgets: {
-        Button: require('./widgets/button'),
-        ButtonGroup: require('./widgets/button-group'),
-        Icon: require('./widgets/icon'),
-        Logo: require('./widgets/logo'),
-        TextButton: require('./widgets/text-button')
+        Button: require('./components/widgets/button'),
+        ButtonGroup: require('./components/widgets/button-group'),
+        Icon: require('./components/widgets/icon'),
+        Logo: require('./components/widgets/logo'),
+        TextButton: require('./components/widgets/text-button')
     }
-}
+};
