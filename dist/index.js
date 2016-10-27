1150ab9f5aee4338ccdbf62bc07c7d06284e1d4c
Jake Josol <jmjosol@hotmail.com>
2016-10-27 17:49:44 +0800

    Fixed packaging

--- dist/index.js
+++ dist/index.js
@@ -1,60 +1,172 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-   value: true
+    value: true
 });
-exports.Page = undefined;
+exports.TextButton = exports.Logo = exports.Icon = exports.ButtonGroup = exports.Button = exports.Table = exports.SideMenu = exports.Side = exports.NavBar = exports.InfoCard = exports.Content = exports.Container = exports.Column = exports.CardLoader = exports.Card = exports.AppBar = exports.Textarea = exports.RadioGroup = exports.Radio = exports.InputNumber = exports.InputMoney = exports.InputGroup = exports.InputFile = exports.InputDate = exports.CheckboxGroup = exports.Checkbox = exports.PieChart = exports.LineGraph = exports.BarStack = exports.Page = undefined;
 
 var _page = require('./components/templates/page');
 
 var _page2 = _interopRequireDefault(_page);
 
+var _barStack = require('./components/charts/bar-stack');
+
+var _barStack2 = _interopRequireDefault(_barStack);
+
+var _lineGraph = require('./components/charts/line-graph');
+
+var _lineGraph2 = _interopRequireDefault(_lineGraph);
+
+var _pieChart = require('./components/charts/pie-chart');
+
+var _pieChart2 = _interopRequireDefault(_pieChart);
+
+var _checkbox = require('./components/form/checkbox');
+
+var _checkbox2 = _interopRequireDefault(_checkbox);
+
+var _checkboxGroup = require('./components/form/checkbox-group');
+
+var _checkboxGroup2 = _interopRequireDefault(_checkboxGroup);
+
+var _inputDate = require('./components/form/input-date');
+
+var _inputDate2 = _interopRequireDefault(_inputDate);
+
+var _inputFile = require('./components/form/input-file');
+
+var _inputFile2 = _interopRequireDefault(_inputFile);
+
+var _inputGroup = require('./components/form/input-group');
+
+var _inputGroup2 = _interopRequireDefault(_inputGroup);
+
+var _inputMoney = require('./components/form/input-money');
+
+var _inputMoney2 = _interopRequireDefault(_inputMoney);
+
+var _inputNumber = require('./components/form/input-number');
+
+var _inputNumber2 = _interopRequireDefault(_inputNumber);
+
+var _radio = require('./components/form/radio');
+
+var _radio2 = _interopRequireDefault(_radio);
+
+var _radioGroup = require('./components/form/radio-group');
+
+var _radioGroup2 = _interopRequireDefault(_radioGroup);
+
+var _textarea = require('./components/form/textarea');
+
+var _textarea2 = _interopRequireDefault(_textarea);
+
+var _appBar = require('./components/layout/app-bar');
+
+var _appBar2 = _interopRequireDefault(_appBar);
+
+var _card = require('./components/layout/card');
+
+var _card2 = _interopRequireDefault(_card);
+
+var _cardLoader = require('./components/layout/card-loader');
+
+var _cardLoader2 = _interopRequireDefault(_cardLoader);
+
+var _column = require('./components/layout/column');
+
+var _column2 = _interopRequireDefault(_column);
+
+var _container = require('./components/layout/container');
+
+var _container2 = _interopRequireDefault(_container);
+
+var _content = require('./components/layout/content');
+
+var _content2 = _interopRequireDefault(_content);
+
+var _infoCard = require('./components/layout/info-card');
+
+var _infoCard2 = _interopRequireDefault(_infoCard);
+
+var _navBar = require('./components/layout/nav-bar');
+
+var _navBar2 = _interopRequireDefault(_navBar);
+
+var _side = require('./components/layout/side');
+
+var _side2 = _interopRequireDefault(_side);
+
+var _sideMenu = require('./components/layout/side-menu');
+
+var _sideMenu2 = _interopRequireDefault(_sideMenu);
+
+var _table = require('./components/layout/table');
+
+var _table2 = _interopRequireDefault(_table);
+
+var _button = require('./components/widgets/button');
+
+var _button2 = _interopRequireDefault(_button);
+
+var _buttonGroup = require('./components/widgets/button-group');
+
+var _buttonGroup2 = _interopRequireDefault(_buttonGroup);
+
+var _icon = require('./components/widgets/icon');
+
+var _icon2 = _interopRequireDefault(_icon);
+
+var _logo = require('./components/widgets/logo');
+
+var _logo2 = _interopRequireDefault(_logo);
+
+var _textButton = require('./components/widgets/text-button');
+
+var _textButton2 = _interopRequireDefault(_textButton);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.Page = _page2.default; /*module.exports = {
-                               
-                                    Templates: {
-                                       Page: require('./components/templates/page')
-                                   },
-                               
-                                    Charts: {
-                                       BarStack: require('./components/charts/bar-stack'),
-                                       LineGraph: require('./components/charts/line-graph'),
-                                       PieChart: require('./components/charts/pie-chart')
-                                   },
-                               
-                                    Form: {
-                                       Checkbox: require('./components/form/checkbox'),
-                                       CheckboxGroup: require('./components/form/checkbox-group'),
-                                       InputDate: require('./components/form/input-date'),
-                                       InputFile: require('./components/form/input-file'),
-                                       InputGroup: require('./components/form/input-group'),
-                                       InputMoney: require('./components/form/input-money'),
-                                       InputNumber: require('./components/form/input-number'),
-                                       Radio: require('./components/form/radio'),
-                                       RadioGroup: require('./components/form/radio-group'),
-                                       Textarea: require('./components/form/textarea')
-                                   },
-                               
-                                    Layout: {
-                                       AppBar: require('./components/layout/app-bar'),
-                                       Card: require('./components/layout/card'),
-                                       CardLoader: require('./components/layout/card-loader'),
-                                       Column: require('./components/layout/column'),
-                                       Container: require('./components/layout/container'),
-                                       Content: require('./components/layout/content'),
-                                       InfoCard: require('./components/layout/info-card'),
-                                       NavBar: require('./components/layout/nav-bar'),
-                                       Side: require('./components/layout/side'),
-                                       SideMenu: require('./components/layout/side-menu'),
-                                       Table: require('./components/layout/table')
-                                   },
-                               
-                                    Widgets: {
-                                       Button: require('./components/widgets/button'),
-                                       ButtonGroup: require('./components/widgets/button-group'),
-                                       Icon: require('./components/widgets/icon'),
-                                       Logo: require('./components/widgets/logo'),
-                                       TextButton: require('./components/widgets/text-button')
-                                   }
-                               };*/
+exports.Page = _page2.default;
+
+// Charts
+// Templates
+
+exports.BarStack = _barStack2.default;
+exports.LineGraph = _lineGraph2.default;
+exports.PieChart = _pieChart2.default;
+
+// Form
+
+exports.Checkbox = _checkbox2.default;
+exports.CheckboxGroup = _checkboxGroup2.default;
+exports.InputDate = _inputDate2.default;
+exports.InputFile = _inputFile2.default;
+exports.InputGroup = _inputGroup2.default;
+exports.InputMoney = _inputMoney2.default;
+exports.InputNumber = _inputNumber2.default;
+exports.Radio = _radio2.default;
+exports.RadioGroup = _radioGroup2.default;
+exports.Textarea = _textarea2.default;
+
+// Layout
+
+exports.AppBar = _appBar2.default;
+exports.Card = _card2.default;
+exports.CardLoader = _cardLoader2.default;
+exports.Column = _column2.default;
+exports.Container = _container2.default;
+exports.Content = _content2.default;
+exports.InfoCard = _infoCard2.default;
+exports.NavBar = _navBar2.default;
+exports.Side = _side2.default;
+exports.SideMenu = _sideMenu2.default;
+exports.Table = _table2.default;
+
+// Widgets
+
+exports.Button = _button2.default;
+exports.ButtonGroup = _buttonGroup2.default;
+exports.Icon = _icon2.default;
+exports.Logo = _logo2.default;
+exports.TextButton = _textButton2.default;
