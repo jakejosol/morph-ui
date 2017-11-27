d2c3c9e55b6dbc8a92d73c99389a19566c81afbc
Jason Josol <josol@mclinica.com>
2017-11-27 15:28:50 +0800

    Updted checkbox label

--- src/components/form/checkbox.js
+++ src/components/form/checkbox.js
@@ -13,6 +13,13 @@ export default class Checkbox extends React.Component {
 		if(this.props.onChange) this.props.onChange(this.props.value);
 	}
 
+	getLabel() {
+		if(this.props.isMarkdown) {
+			return <Markdown source={this.props.label} />;
+		}
+		else return this.props.label;
+	}
+
 	render() {
 		return <label className='checkbox' htmlFor={'checkbox-' + this.props.name + '-' + this.props.value}>
 			<input 
@@ -22,7 +29,7 @@ export default class Checkbox extends React.Component {
 				name={this.props.name + '[]'} 
 				checked={typeof this.props.active == 'undefined' ? this.props.defaultValue : this.props.active}
 				onChange={this.onChange} />
-			<span><Markdown source={this.props.label} /></span>
+			<span>{this.getLabel()}</span>
 		</label>;
 	}
 }
